d9a9e837d23e28fc039b110b941f318f577e367d
VadVels <v.veselovskiy28@gmail.com>
2025-11-13 10:21:19 +0300

    Драйвер базы данных 2

--- addressbook_tests_web/src/test/java/tests/NewGroupTest.java
+++ addressbook_tests_web/src/test/java/tests/NewGroupTest.java
@@ -42,18 +42,19 @@ public class NewGroupTest extends TestBase {
     }
 
     @ParameterizedTest
-    @MethodSource("groupProvider")
-    public void newMultipleGroup(GroupData group) {
+    @MethodSource("singleRandomGroup")
+    public void canCreateGroup(GroupData group) {
         var oldGroups = app.jdbc().getGroupList();
         app.groups().newGroup(group);
-        var newGroups = app.groups().getList();
+        var newGroups = app.jdbc().getGroupList();
         Comparator<GroupData> compareById = (o1, o2) -> {
             return Integer.compare(Integer.parseInt(o1.id()), Integer.parseInt(o2.id()));
         };
         newGroups.sort(compareById);
+        var maxId = newGroups.get(newGroups.size()- 1).id();
 
         var expectedList = new ArrayList<>(oldGroups);
-        expectedList.add(group.withId(newGroups.get(newGroups.size() - 1).id()).withHeader("").withFooter(""));
+        expectedList.add(group.withId(maxId));
         expectedList.sort(compareById);
         Assertions.assertEquals(newGroups, expectedList);
 
